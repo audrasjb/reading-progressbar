362eb65da4de2bccba01a4dfcc2dabcf1b347ab7
audrasjb <[audrasjb@gmail.com]>
2016-12-22 22:23:07 +0100

    still init

---
+++ admin/rp-admin.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://jeanbaptisteaudras.com
+ * @since      1.0.0
+ *
+ * @package    reading-progressbar
+ * @subpackage reading-progressbar/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @package    reading-progressbar
+ * @subpackage reading-progressbar/admin
+ * @author     audrasjb <audrasjb@gmail.com>
+ */
+	// Enqueue styles
+	add_action( 'admin_enqueue_scripts', 'enqueue_styles_reading_progressbar_admin' );
+	function enqueue_styles_reading_progressbar() {
+		wp_enqueue_style( 'reading_progressbar', plugin_dir_url( __FILE__ ) . 'css/rp-admin.css', array(), '', 'all' );
+	}
+	
+	// Enqueue scripts
+	add_action( 'admin_enqueue_scripts', 'enqueue_scripts_reading_progressbar_admin' );
+	function enqueue_scripts_reading_progressbar() {
+		wp_enqueue_script( 'asagenda', plugin_dir_url( __FILE__ ) . 'js/rp-admin.js', array( 'jquery', 'wp-color-picker' ), '', false );
+	}
+	
+	
--- reading-progressbar.php
+++ reading-progressbar.php
@@ -3,18 +3,18 @@
 /**
  * @link              http://jeanbaptisteaudras.com/wstjb-bouton-justifier-texte-wordpress/
  * @since             1.0
- * @package           Who Stole the Text Justify Button ?!
+ * @package           Reading progressbar
  *
  * @wordpress-plugin
- * Plugin Name:       Who Stole the Text Justify Button ?!
- * Plugin URI:        http://jeanbaptisteaudras.com/wstjb-bouton-justifier-texte-wordpress/
- * Description:       OMG! WordPress 4.7 stole my text justify button! Please bring it back :)
+ * Plugin Name:       Reading progressbar
+ * Plugin URI:        http://jeanbaptisteaudras.com
+ * Description:       Reading progressbar WIP
  * Version:           1.0
  * Author:            Jean-Baptiste Audras, project manager @ Whodunit
- * Author URI:        http://jeanbaptisteaudras.com/
+ * Author URI:        http://jeanbaptisteaudras.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wstjb
+ * Text Domain:       rp
  * Domain Path:       /languages
  */
 
@@ -23,30 +23,18 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-class WSTJB {
-	function __construct() {
-		if ( is_admin() ) {
-			add_action( 'init', array(  $this, 'setup_WSTJB' ) );
-		}
-	}
-	function setup_WSTJB() {
-		// Check if the logged in WordPress User can edit Posts or Pages
-		// If not, don't register
-		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
-        	return;
-		}
-		// Check if the logged in WordPress User has the Visual Editor enabled
-		// If not, don't register
-		if ( get_user_option( 'rich_editing' ) !== 'true' ) {
-			return;
-		}
-		// Add text justify button again…
-		function mce_WSTJB( $mce_buttons ) {	
-			$mce_buttons[] = 'alignjustify';
-			return $mce_buttons;
-		}	
-		add_filter( 'mce_buttons_2', 'mce_WSTJB', 5 );
-		// Celebrate! =D
-	}
+/**
+ * i18n
+ */
+//require_once plugin_dir_path( dirname( __FILE__ ) ) . '/' .$plugin_name . '/includes/rp-i18n.php';
+
+/**
+ * Admin
+ */
+require_once plugin_dir_path( dirname( __FILE__ ) ) . '/' .$plugin_name . '/admin/rp-admin.php';
+
+/**
+ * Public
+ */
+//require_once plugin_dir_path( dirname( __FILE__ ) ) . '/' .$plugin_name . '/public/asagenda-public.php';
 }
-$WSTJB = new WSTJB;
